e35efeda13411e899c9d257bd2be508fd8e7cb59
b0b3t0 <cruelbobo@abv.bg>
2020-08-03 15:55:50 +0300

    Added Taco entity class, DesignTacoController and design html page

---
+++ src/main/java/com/demo/web/DesignTacoController.java
@@ -0,0 +1,53 @@
+package com.demo.web;
+
+import com.demo.Ingredient;
+import com.demo.Taco;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                new Ingredient("TMTO", "Deced Tomatoes", Ingredient.Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+        );
+
+        Ingredient.Type[] types = Ingredient.Type.values();
+        for (Ingredient.Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+        model.addAttribute("design", new Taco());
+
+        return "design";
+    }
+
+    @PostMapping
+    public String processDesign(Design desing) {
+        //Save the taco design
+        //We'll do this in next lesson
+        log.info("Processing design: " + desing);
+
+        return "redirect:/orders/current";
+    }
+}
--- src/main/java/com/demo/web/HomeController.java
+++ src/main/java/com/demo/web/HomeController.java
@@ -1,4 +1,4 @@
-package com.demo;
+package com.demo.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
