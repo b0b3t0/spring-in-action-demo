a839e29ef8905fb1a651420ae0dd481e84530133
b0b3t0 <cruelbobo@abv.bg>
2020-08-03 17:58:50 +0300

    Added validator dependency and add validation anotations in Taco entity class

--- src/main/java/com/demo/data/Taco.java
+++ src/main/java/com/demo/data/Taco.java
@@ -2,11 +2,15 @@ package com.demo.data;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
 
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
     private List<String> ingredients;
 }
