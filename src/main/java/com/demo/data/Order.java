77d0e12e8f622261560d15204725c85ea5c05541
b0b3t0 <cruelbobo@abv.bg>
2020-08-04 13:31:19 +0300

    Refactored OrderController by injecting orderRepository and added saveDesign method in Order entity class

--- src/main/java/com/demo/data/Order.java
+++ src/main/java/com/demo/data/Order.java
@@ -6,13 +6,15 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class Order {
 
     private Long id;
-    private Date createdAt;
+    private Date placedAt;
     @NotBlank(message = "Name is required")
     private String name;
     @NotBlank(message = "Street is required")
@@ -30,5 +32,9 @@ public class Order {
     private String ccExpiration;
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
+    List<Taco> tacos = new ArrayList<>();
 
+    public void saveDesign(Taco design) {
+        this.tacos.add(design);
+    }
 }
--- src/main/java/com/demo/web/OrderController.java
+++ src/main/java/com/demo/web/OrderController.java
@@ -1,6 +1,7 @@
 package com.demo.web;
 
 import com.demo.data.Order;
+import com.demo.data.repositories.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,28 +9,35 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
 
-    @GetMapping("/current")
-    public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
+    private final OrderRepository orderRepository;
 
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @GetMapping("/current")
+    public String orderForm() {
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
-
-        log.info("Order submitted: " + order);
+        orderRepository.save(order);
+        sessionStatus.setComplete();
 
         return "redirect:/";
     }
