823eb3137d74ae1fabf2b9353c9206a602077eea
b0b3t0 <cruelbobo@abv.bg>
2020-08-04 12:38:04 +0300

    Refactored DesignTacoController showDesignForm method

--- src/main/java/com/demo/web/DesignTacoController.java
+++ src/main/java/com/demo/web/DesignTacoController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
