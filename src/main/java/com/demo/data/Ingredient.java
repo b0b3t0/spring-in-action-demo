8b7228507e9c17056907abca1c6d3d513575238d
b0b3t0 <cruelbobo@abv.bg>
2020-08-19 01:09:39 +0300

    Added @Entity in Taco, Order and Ingredient classes

--- src/main/java/com/demo/data/Ingredient.java
+++ src/main/java/com/demo/data/Ingredient.java
@@ -1,15 +1,24 @@
 package com.demo.data;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Entity
 public class Ingredient {
 
-    private final String id;
-    private final String name;
-    private final Type type;
+    @Id
+    private String id;
+    private String name;
+    private Type type;
+
 
     public static enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
--- src/main/java/com/demo/data/Order.java
+++ src/main/java/com/demo/data/Order.java
@@ -3,6 +3,7 @@ package com.demo.data;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -10,9 +11,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Entity
 @Data
+@Table(name = "taco_order")
 public class Order {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date placedAt;
     @NotBlank(message = "Name is required")
@@ -32,9 +39,16 @@ public class Order {
     private String ccExpiration;
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
-    List<Taco> tacos = new ArrayList<>();
+
+    @ManyToMany(targetEntity = Taco.class)
+    private List<Taco> tacos = new ArrayList<>();
 
     public void saveDesign(Taco design) {
         this.tacos.add(design);
+    }
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
     }
 }
