921bc82a3c4bc276948b65503625a89dcfc4ca97
b0b3t0 <bnenov91@gmail.com>
2020-08-29 23:10:57 +0300

    Renamed the predicate in OrderRepository methods

--- src/main/java/com/demo/data/repositories/OrderRepository.java
+++ src/main/java/com/demo/data/repositories/OrderRepository.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
-    List<Order> findByDeliveryZip(String deliveryZip);
+    List<Order> findByZip(String deliveryZip);
 
-    List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(
+    List<Order> readOrdersByZipAndPlacedAtBetween(
             String deliveryZip, Date startDate, Date endDate);
     
 }
