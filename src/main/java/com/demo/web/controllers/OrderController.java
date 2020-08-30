5ff01918d054a142d3aed7fe03da505c5d38366e
b0b3t0 <bnenov91@gmail.com>
2020-08-30 12:15:08 +0300

    Added Principal in OrderController

--- src/main/java/com/demo/web/controllers/OrderController.java
+++ src/main/java/com/demo/web/controllers/OrderController.java
@@ -1,7 +1,9 @@
 package com.demo.web.controllers;
 
 import com.demo.data.models.Order;
+import com.demo.data.models.User;
 import com.demo.data.repositories.OrderRepository;
+import com.demo.data.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
+import java.security.Principal;
 
 @Slf4j
 @Controller
@@ -20,9 +23,11 @@ import javax.validation.Valid;
 public class OrderController {
 
     private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
 
-    public OrderController(OrderRepository orderRepository) {
+    public OrderController(OrderRepository orderRepository, UserRepository userRepository) {
         this.orderRepository = orderRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/current")
@@ -31,12 +36,17 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid Order order, Errors errors,
+                               SessionStatus sessionStatus, Principal principal) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
         orderRepository.save(order);
         sessionStatus.setComplete();
+
+        User user = userRepository
+                .findByUsername(principal.getName());
+        order.setUser(user);
 
         return "redirect:/";
     }
