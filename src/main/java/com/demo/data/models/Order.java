a7044c29eb688aa9aa11a7c6eb6f8d7f12e34648
b0b3t0 <bnenov91@gmail.com>
2020-08-30 12:10:00 +0300

    Added User field in Order class with relation many-to-one

--- src/main/java/com/demo/data/models/Order.java
+++ src/main/java/com/demo/data/models/Order.java
@@ -40,6 +40,9 @@ public class Order {
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
+    @ManyToOne
+    private User user;
+
     @ManyToMany(targetEntity = Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
