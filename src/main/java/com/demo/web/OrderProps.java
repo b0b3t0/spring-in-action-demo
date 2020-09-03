267cc6a53b31e117ac2d9d784cbac6fca6e8d8ef
b0b3t0 <bnenov91@gmail.com>
2020-09-03 23:27:18 +0300

    Created OrderProps class to take care of pagesize and added it into OrderController

---
+++ src/main/java/com/demo/web/OrderProps.java
@@ -0,0 +1,13 @@
+package com.demo.web;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "taco.orders")
+@Data
+public class OrderProps {
+
+    private int pageSize = 20;
+}
--- src/main/java/com/demo/web/controllers/OrderController.java
+++ src/main/java/com/demo/web/controllers/OrderController.java
@@ -4,8 +4,8 @@ import com.demo.data.models.Order;
 import com.demo.data.models.User;
 import com.demo.data.repositories.OrderRepository;
 import com.demo.data.repositories.UserRepository;
+import com.demo.web.OrderProps;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -25,19 +25,14 @@ import java.security.Principal;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
-@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
 
-    private int pageSize = 20;
-
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-
+    private final OrderProps props;
     private final OrderRepository orderRepository;
     private final UserRepository userRepository;
 
-    public OrderController(OrderRepository orderRepository, UserRepository userRepository) {
+    public OrderController(OrderProps props, OrderRepository orderRepository, UserRepository userRepository) {
+        this.props = props;
         this.orderRepository = orderRepository;
         this.userRepository = userRepository;
     }
@@ -65,7 +60,7 @@ public class OrderController {
 
     @GetMapping
     public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
-        Pageable pageable = PageRequest.of(0, pageSize);
+        Pageable pageable = PageRequest.of(0, props.getPageSize());
         model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
 
         return "orderList";
