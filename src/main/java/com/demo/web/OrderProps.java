7d44393f16c80bfcca15a38a6f646a8d23ecba18
b0b3t0 <bnenov91@gmail.com>
2020-09-03 23:32:20 +0300

    Added validation for pagesize between 5 and 25

--- src/main/java/com/demo/web/OrderProps.java
+++ src/main/java/com/demo/web/OrderProps.java
@@ -3,11 +3,18 @@ package com.demo.web;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 @Component
 @ConfigurationProperties(prefix = "taco.orders")
 @Data
+@Validated
 public class OrderProps {
 
+    @Min(value = 5, message = "Can not be less than 5")
+    @Max(value = 25, message = "Can not be more than 25")
     private int pageSize = 20;
 }
