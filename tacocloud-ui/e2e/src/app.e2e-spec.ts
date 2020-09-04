26beacaa15efbef8520ac8adb9cbe9316815c253
b0b3t0 <bnenov91@gmail.com>
2020-09-04 15:09:58 +0300

    Setting Angular front-end

--- tacocloud-ui/e2e/src/app.e2e-spec.ts
+++ tacocloud-ui/e2e/src/app.e2e-spec.ts
@@ -1,7 +1,6 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('taco-web-ui App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -10,14 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('tacocloud-ui app is running!');
-  });
-
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
